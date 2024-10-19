6bdcf080dc7d5ba6911ad5db075bb8d564f8997c
Kamal Nayan Upadhyay <knupadhyay784@gmail.com>
2024-10-19 15:44:22 +0530

    done

--- src/components/scrollTop/scrollTop.jsx
+++ src/components/scrollTop/scrollTop.jsx
@@ -8,7 +8,7 @@ const ScrollTop = () => {
         /* settimeout make sure this run after components have rendered. This will help fixing bug for some views where scroll to top not working perfectly */
         setTimeout(() => {
             // window.scrollTo({ top: 0, behavior: 'smooth' })
-            window.scrollTo(0, 0)
+            window.scrollTo({top: 0, behavior: 'smooth'})
         }, 0)
     }, [pathname])
     return null
