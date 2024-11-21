71719dcc1f12480d703707c3ac86ca9c58ed0a15
Kamal Nayan Upadhyay <knupadhyay784@gmail.com>
2024-11-21 17:53:57 +0530

    done

--- src/pages/checkoutPage/CheckoutPage.jsx
+++ src/pages/checkoutPage/CheckoutPage.jsx
@@ -1147,11 +1147,11 @@ const CartPage = () => {
     return (
         <Layout>
             <div className="container mx-auto max-w-7xl px-2 lg:px-0">
-                <pre>{JSON.stringify(formData, null, 2)}</pre>
-                <pre>{JSON.stringify(error?.data, null, 2)}</pre>
-                <pre>{JSON.stringify(formData.adharcardImg, null, 2)}</pre>
-                {isError && <pre>{JSON.stringify("error", error, null, 2)}</pre>}
-                <pre>{JSON.stringify("Verify payment Error",verifyPaymentError,null,2)}</pre>
+                {/* <pre>{JSON.stringify(formData, null, 2)}</pre> */}
+                {/* <pre>{JSON.stringify(error?.data, null, 2)}</pre> */}
+                {/* <pre>{JSON.stringify(formData.adharcardImg, null, 2)}</pre> */}
+                {/* {isError && <pre>{JSON.stringify("error", error, null, 2)}</pre>} */}
+                {/* <pre>{JSON.stringify("Verify payment Error",verifyPaymentError,null,2)}</pre> */}
 
                 <div className="mx-auto max-w-2xl lg:max-w-7xl">
                     <form className="lg:mt-10 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-12">
